4fba38963b0d4a2e693638c290005cd6c5659e58
tu <tu1231230@gmail.com>
2024-05-05 21:05:22 +0700

    w

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,129 +1,219 @@
-'use client'
-import { icon } from "./icon"
-import { button } from "./button/button"
-import './header.scss'
-import { useEffect, useRef, useState } from "react"
-import Login from "./login/login"
-import Link from "next/link"
-import Cookies from "js-cookie"
-import { Avatar, getUser } from "./home/content"
-import { getData, getData2, updateData } from "./firebase/config"
-import { documentId, where } from "firebase/firestore"
-const NotiItem=({value}:{value:any})=>{
-    return <div className="NotiItem">
-    <Avatar img={value.userSend.imgURL}></Avatar> <div className="NotiItem-content">
-        {value.userSend.name}<br></br>
-        <span style={{
-            color:'white'
-        }}>{value.message}</span></div>
+"use client";
+import { icon } from "./icon";
+import { button } from "./button/button";
+import "./header.scss";
+import { useEffect, useRef, useState } from "react";
+import Login from "./login/login";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import { Avatar, getUser } from "./home/content";
+import { getData, getData2, updateData } from "./firebase/config";
+import { documentId, where } from "firebase/firestore";
+const NotiItem = ({ value }: { value: any }) => {
+  return (
+    <div className="NotiItem">
+      <Avatar img={value.userSend.imgURL}></Avatar>{" "}
+      <div className="NotiItem-content">
+        {value.userSend.name}
+        <br></br>
+        <span
+          style={{
+            color: "white",
+          }}
+        >
+          {value.message}
+        </span>
+      </div>
     </div>
-}
-const NotificationEl=({listNott}:{listNott:any})=>{
-    if(listNott.length==0)
-        return  <div className="notification">
-            Chưa có thông báo mới
-       
+  );
+};
+const NotificationEl = ({ listNott }: { listNott: any }) => {
+  if (listNott.length == 0)
+    return <div className="notification">Chưa có thông báo mới</div>;
+  return (
+    <div className="notification">
+      {listNott.map((i: any) => {
+        return <NotiItem value={i} key={i.id}></NotiItem>;
+      })}
     </div>
-    return     <div className="notification">
-    {listNott.map((i:any)=>{
-    return  <NotiItem value={i} key={i.id}></NotiItem>
-    })}
-       
-    </div>
-}
-export default  function Header(){
-    const [listNott,setListNoti]=useState([])
-    useEffect(()=>{
-        if(getUser())
-        getData('thongbaotinnhan',(e:any)=>{
-           setListNoti(e)
-        },where('userid','==',getUser().id))
-    },[])
-    const [login,setLogin]=useState(false)
-    const [openNoti,setOpenNoti]=useState<boolean>(false)
-    const menu=useRef<any>()
-    const [user,setUser]=useState<any>({name:''})
-    useEffect(()=>{
-        window.addEventListener('click',(e:any)=>{
-       
-            if(e.target.className=='user-button'||e.target.className=='opennoti')
-                {
-                    setOpenNoti(true)
-                }
-            if((e.target.className!='user-button'&&e.target.className!='opennoti')&&e.target.className!='NotiItem'&&e.target.className!='NotiItem-content'&&e.target.className!='notification')
-                {
-                    setOpenNoti(false)
-                }
-        })
-        if(getUser())
-           { updateData('user',getUser().id,{online:true},()=>{
-                getData2('user',(e:any)=>{
-                
-                    setUser(e[0])
-                },where(documentId(),'==',getUser()?.id))
-            },()=>{})
-            window.addEventListener("beforeunload", (ev) => 
-                {  
-                    updateData('user',getUser().id,{online:false},()=>{
-                       
-                    },()=>{})
-                });}
-    },[])
-    if(user!=null)
-    return <div className="header-component">
-        <div ref={menu}className="menu2">
-        <div className="menu-option">
-        <div className="menu-option-header">
-        <div className="logo-img">
+  );
+};
+export default function Header() {
+  const [listNott, setListNoti] = useState([]);
+  useEffect(() => {
+    if (getUser())
+      getData(
+        "thongbaotinnhan",
+        (e: any) => {
+          setListNoti(e);
+        },
+        where("userid", "==", getUser().id)
+      );
+  }, []);
+  const [login, setLogin] = useState(false);
+  const [openNoti, setOpenNoti] = useState<boolean>(false);
+  const menu = useRef<any>();
+  const [user, setUser] = useState<any>({ name: "" });
+  useEffect(() => {
+    window.addEventListener("click", (e: any) => {
+      if (
+        e.target.className == "user-button" ||
+        e.target.className == "opennoti"
+      ) {
+        setOpenNoti(true);
+      }
+      if (
+        e.target.className != "user-button" &&
+        e.target.className != "opennoti" &&
+        e.target.className != "NotiItem" &&
+        e.target.className != "NotiItem-content" &&
+        e.target.className != "notification"
+      ) {
+        setOpenNoti(false);
+      }
+    });
+    if (getUser()) {
+      updateData(
+        "user",
+        getUser().id,
+        { online: true },
+        () => {
+          getData2(
+            "user",
+            (e: any) => {
+              setUser(e[0]);
+            },
+            where(documentId(), "==", getUser()?.id)
+          );
+        },
+        () => {}
+      );
+      window.addEventListener("beforeunload", (ev) => {
+        updateData(
+          "user",
+          getUser().id,
+          { online: false },
+          () => {},
+          () => {}
+        );
+      });
+    }
+  }, []);
+  if (user != null)
+    return (
+      <div className="header-component">
+        <div ref={menu} className="menu2">
+          <div className="menu-option">
+            <div className="menu-option-header">
+              <div className="logo-img">
                 <img src={icon.logo.src}></img>
-              
+              </div>
+              <div className="logo-close">
+                <img
+                  onClick={() => {
+                    menu.current.style.transform = "translateX(-100vw)";
+                  }}
+                  src={icon.closewhite.src}
+                ></img>
+              </div>
             </div>
-            <div className="logo-close"><img onClick={()=>{
-            menu.current.style.transform='translateX(-100vw)'
-            }} src={icon.closewhite.src}></img></div>
-        </div>
-        <Link href={'/'}>
-            <div  className="bottion-icon">          <img src={icon.home.src}></img>Home</div>
+            <Link href={"/"}>
+              <div className="bottion-icon">
+                {" "}
+                <img src={icon.home.src}></img>Home
+              </div>
             </Link>
-            <div className="bottion-icon">          <img src={icon.group.src}></img>Group</div>
-            <div className="bottion-icon">          <img src={icon.video.src}></img>Videos</div>
-            <div className="bottion-icon">          <img src={icon.market.src}></img>MarketPlace</div>
+            <div className="bottion-icon">
+              {" "}
+              <img src={icon.group.src}></img>Group
+            </div>
+            <div className="bottion-icon">
+              {" "}
+              <img src={icon.video.src}></img>Videos
+            </div>
+            <div className="bottion-icon">
+              {" "}
+              <img src={icon.market.src}></img>MarketPlace
+            </div>
+          </div>
+          <div className="menu-close"></div>
         </div>
-        <div className="menu-close"></div>
-        </div>
-          {login&&<Login onClose={()=>setLogin(false)}></Login>}
+        {login && <Login onClose={() => setLogin(false)}></Login>}
         <div className="logo">
-            <div className="logo-img">
-                <img src={icon.logo.src}></img>
-            </div>
-            <div className="logo-search"></div>
+          <div className="logo-img">
+            <img src={icon.logo.src}></img>
+          </div>
+          <div className="logo-search"></div>
         </div>
-     
+
         <div className="button-header">
-        <div onClick={()=>{
-       menu.current.style.transform='translateX(0)'
-        }} className="bottion-icon menu">          <img src={icon.menu.src}></img></div>
-            <Link href={'/'}>
-            <div  className="bottion-icon">          <img src={icon.home.src}></img></div>
-            </Link>
-          
-            <div className="bottion-icon">          <img src={icon.group.src}></img></div>
-            <div className="bottion-icon">          <img src={icon.video.src}></img></div>
-            <div className="bottion-icon">          <img src={icon.market.src}></img></div>
+          <div
+            onClick={() => {
+              menu.current.style.transform = "translateX(0)";
+            }}
+            className="bottion-icon menu"
+          >
+            {" "}
+            <img src={icon.menu.src}></img>
+          </div>
+          <Link href={"/"}>
+            <div className="bottion-icon">
+              {" "}
+              <img src={icon.home.src}></img>
+            </div>
+          </Link>
+
+          <div className="bottion-icon">
+            {" "}
+            <img src={icon.group.src}></img>
+          </div>
+          <div className="bottion-icon">
+            {" "}
+            <img src={icon.video.src}></img>
+          </div>
+          <div className="bottion-icon">
+            {" "}
+            <img src={icon.market.src}></img>
+          </div>
         </div>
         <div className="user-icon">
-            {!user?.name?  <div onClick={()=>setLogin(true)} className="bottion-icon"> Đăng nhập</div>:
-            <><div className="user-button">          <img className="opennoti" src={icon.mess.src}></img>
-           {listNott.length>0&& <div className="notinumber">
-                {listNott.length}
-            </div>}
-       {openNoti&& <NotificationEl listNott={listNott}></NotificationEl>}
+          {!user?.name ? (
+            <div onClick={() => setLogin(true)} className="bottion-icon">
+              {" "}
+              Đăng nhập
             </div>
-            <div className="user-button">          <img src={icon.bell.src}></img></div>
-      
-            <div className="user-avatar">     <Link href={'/'+user.id}> <img src={ user?.imgURL||'https://fptshop.com.vn/uploads/originals/2023/11/14/638356007024019763_anime-ai-la-gi-bat-mi-cach-tao-anime-bang-ai-cuc-don-gian.png'}></img></Link>    </div>
-            </>}
-        
+          ) : (
+            <>
+              <div className="user-button">
+                {" "}
+                <img className="opennoti" src={icon.mess.src}></img>
+                {listNott.length > 0 && (
+                  <div className="notinumber">{listNott.length}</div>
+                )}
+                {openNoti && (
+                  <NotificationEl listNott={listNott}></NotificationEl>
+                )}
+              </div>
+              <div className="user-button">
+                {" "}
+                <img src={icon.bell.src}></img>
+              </div>
+
+              <div className="user-avatar">
+                {" "}
+                <Link href={"/" + user.id}>
+                  {" "}
+                  <img
+                    src={
+                      user?.imgURL ||
+                      "https://fptshop.com.vn/uploads/originals/2023/11/14/638356007024019763_anime-ai-la-gi-bat-mi-cach-tao-anime-bang-ai-cuc-don-gian.png"
+                    }
+                  ></img>
+                </Link>{" "}
+              </div>
+            </>
+          )}
         </div>
-    </div>
+      </div>
+    );
 }
--- src/component/messenger/messenger.tsx
+++ src/component/messenger/messenger.tsx
@@ -1,179 +1,281 @@
-'use client'
-import Image from 'next/image'
-import { Avatar, CommentInput } from '../home/content'
-import './messenger.scss'
-import { icon } from '../icon'
-import { ReactNode, memo, useContext, useEffect, useRef, useState } from 'react'
-import { addData, getData, getData2, updateData } from '../firebase/config'
-import { and, documentId, where } from 'firebase/firestore'
-import { HomeContext } from '@/app/page'
-import moment from 'moment'
-import { v4 } from 'uuid'
-import Link from 'next/link'
-import { Loading2 } from '../loading/loading'
-import { message } from 'antd'
+"use client";
+import Image from "next/image";
+import { Avatar, CommentInput } from "../home/content";
+import "./messenger.scss";
+import { icon } from "../icon";
+import {
+  ReactNode,
+  memo,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { addData, getData, getData2, updateData } from "../firebase/config";
+import { and, documentId, where } from "firebase/firestore";
+import { HomeContext } from "@/app/page";
+import moment from "moment";
+import { v4 } from "uuid";
+import Link from "next/link";
+import { Loading2 } from "../loading/loading";
+import { message } from "antd";
 
-
-
-const MessItem=({value,list,index}:{value:any,list:any,index:number})=>{
-    let item=list[index-1]
-    const {user}=useContext(HomeContext)
-    return <div className={`MessItem ${value.id==user.id&&'userchat'}`}>
-       {value.id==user.id?<></>:item&&item.id==value.id?<div style={{
-        minWidth:30
-       }}></div>: <Avatar img={value.imgURL}></Avatar>}
-        <div className='MessItem-content'>
-            <p>{value.chat}</p>
-            <div className='MessItem-content-time'>{moment(value.time,'DD/MM/YYYY HH:mm').toNow()}</div>
+const MessItem = ({
+  value,
+  list,
+  index,
+}: {
+  value: any;
+  list: any;
+  index: number;
+}) => {
+  let item = list[index - 1];
+  const { user } = useContext(HomeContext);
+  return (
+    <div className={`MessItem ${value.id == user.id && "userchat"}`}>
+      {value.id == user.id ? (
+        <></>
+      ) : item && item.id == value.id ? (
+        <div
+          style={{
+            minWidth: 30,
+          }}
+        ></div>
+      ) : (
+        <Avatar img={value.imgURL}></Avatar>
+      )}
+      <div className="MessItem-content">
+        <p>{value.chat}</p>
+        <div className="MessItem-content-time">
+          {moment(value.time, "DD/MM/YYYY HH:mm").toNow()}
         </div>
+      </div>
     </div>
-}
-const MessInput=({data,value}:{data:any,value:any})=>{
-    const [text,setText]=useState<string>('')
-    const {user}=useContext(HomeContext)
-    const onSend=(e?:any)=>{
-        if(e) e.preventDefault()
-            if(text.trim()=='')return
-       setText('')
-        updateData('messenger',data?.id,{
-            chats:[...data.chats,{...user,chat:text,idchat:v4(),time:moment().format('DD/MM/YYYY HH:mm')}]
-        },()=>{
-            getData2('thongbaotinnhan',(e:any)=>{
-                let find=e.find((i:any)=>i.userSend.id==user.id)
-                if(find)
-                    {
-                        updateData('thongbaotinnhan',find.id,{
-                            message:text
-                        },()=>{
+  );
+};
+const MessInput = ({ data, value }: { data: any; value: any }) => {
+  const [text, setText] = useState<string>("");
+  const { user } = useContext(HomeContext);
+  const onSend = (e?: any) => {
+    if (e) e.preventDefault();
+    if (text.trim() == "") return;
+    setText("");
+    updateData(
+      "messenger",
+      data?.id,
+      {
+        chats: [
+          ...data.chats,
+          {
+            ...user,
+            chat: text,
+            idchat: v4(),
+            time: moment().format("DD/MM/YYYY HH:mm"),
+          },
+        ],
+      },
+      () => {
+        getData2(
+          "thongbaotinnhan",
+          (e: any) => {
+            let find = e.find((i: any) => i.userSend.id == user.id);
+            if (find) {
+              updateData(
+                "thongbaotinnhan",
+                find.id,
+                {
+                  message: text,
+                },
+                () => {},
+                () => {}
+              );
+            } else {
+              addData(
+                "thongbaotinnhan",
+                {
+                  message: text,
+                  userid: value.id,
+                  userSend: { ...user },
+                  read: false,
+                  time: moment().format("DD/MM/YYYY HH:mm"),
+                },
+                () => {}
+              );
+            }
+            updateData(
+              "realtime",
+              "08ORLO3C2HSF4aCTZwcf",
+              {
+                user: { ...value },
+                userSend: { ...user },
+                time: moment().format("DD/MM/YYYY HH:mm"),
+              },
+              () => {},
+              () => {}
+            );
+          },
+          where("userid", "==", value.id)
+        );
+      },
+      () => {}
+    );
+  };
+  return (
+    <div className="MessInput">
+      <textarea
+        autoFocus
+        onKeyPress={(e: any) => {
+          if (e.key == "Enter" && e.shiftKey) return;
 
-                        },()=>{})
-                    }
-                else{
-                    addData('thongbaotinnhan',{
-                        message:text,
-                        userid:value.id,
-                        userSend:{...user},
-                        read:false,
-                        time:moment().format('DD/MM/YYYY HH:mm')
-                    },()=>{})
-                }
-                updateData('realtime','08ORLO3C2HSF4aCTZwcf',{user:{...value},userSend:{...user},time:moment().format('DD/MM/YYYY HH:mm')},()=>{},()=>{})
-            },where('userid','==',value.id))
-        },()=>{})
+          if (e.key == "Enter") {
+            onSend(e);
+          }
+        }}
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        rows={1}
+      ></textarea>{" "}
+      <Image
+        onClick={() => {
+          onSend();
+        }}
+        alt=""
+        width={20}
+        height={20}
+        src={icon.send.src}
+      ></Image>
+    </div>
+  );
+};
+const Messenger = ({ value }: { value: any }) => {
+  const { user, setListChat } = useContext(HomeContext);
+  const [chat, setChat] = useState<any>({});
+  const [loading, setLoading] = useState(true);
+  const count = useRef(1);
+  const getDataChat = () => {
+    if (user)
+      getData(
+        "messenger",
+        (e: any) => {
+          setLoading(false);
+          if (e[0])
+            setChat(
+              e.find((i: any) => i.member.find((a: string) => a == value.id))
+            );
+        },
+        where("member", "array-contains", user.id)
+      );
+  };
+
+  useEffect(() => {
+    if (count.current == 1 && user) {
+      getData2(
+        "messenger",
+        (e: any) => {
+          if (!e?.find((i: any) => i.member.find((a: string) => a == i.id))) {
+            addData(
+              "messenger",
+              {
+                member: [user.id, value.id],
+                chats: [],
+              },
+              () => {
+                getDataChat();
+              }
+            );
+          } else getDataChat();
+        },
+        where("member", "array-contains", user.id)
+      );
     }
-    return <div className='MessInput'>
-        <textarea autoFocus onKeyPress={(e:any)=>{
-        if(e.key=='Enter'&&e.shiftKey) return
-        
-        if(e.key=='Enter'){
-            onSend(e)
-        }
-       }} value={text} onChange={(e)=>{
-            setText(e.target.value)
-        }} rows={1}></textarea> <Image onClick={()=>{
-            onSend()
-        }} alt='' width={20} height={20} src={icon.send.src} ></Image>
-    </div>
-}
- const Messenger=({value}:{
-    value:any
-})=>{
-    const {user,setListChat}=useContext(HomeContext)
-    const [chat,setChat]=useState<any>({})
-    const [loading,setLoading]=useState(true)
-    const count=useRef(1)
-    const getDataChat=()=>{
-        if(user)
-        getData('messenger',((e:any)=>{
-            setLoading(false)
-            if(e[0])
-            setChat(e.find((i:any)=>i.member.find((a:string)=>a==value.id)))
-            
-                
-            
-                   
-                }),where('member','array-contains',user.id))
-           }
-    
-    useEffect(()=>{
-        if(count.current==1&&user)
-       {
-        
-                getData2('messenger',((e:any)=>{
-    
-                        if(!e?.find((i:any)=>i.member.find((a:string)=>a==i.id)))
-                            {
-                           
-                            addData('messenger',{
-                                member:[user.id,value.id],
-                                chats:[]
-                            },()=>{    
 
-                                getDataChat()  
-                            })}
-                            else getDataChat()
-     
-                            }),where('member','array-contains',user.id)
-                         
-                    
-                    )}
-  
-        count.current=2
-       return ()=>{}
-    },[])
-   
-    return <div className='Messenger'>
+    count.current = 2;
+    return () => {};
+  }, []);
 
-        <div className={`Messenger-header`}>
-            <Link href={'/'+value.id}><Avatar img={value.imgURL}></Avatar></Link>
-            <p> {value.name}</p>
-            <div className='Messenger-button'>
-                <Image onClick={()=>{
-                    setListChat((pre:any)=>{
-                        return pre.filter((i:any)=>i.id!=value.id)
-                    })
-                }} alt='' className='img' width={30} height={30} src={icon.closewhite.src}></Image>
-            </div>
+  return (
+    <div className="Messenger">
+      <div className={`Messenger-header`}>
+        <Link href={"/" + value.id}>
+          <Avatar img={value.imgURL}></Avatar>
+        </Link>
+        <p> {value.name}</p>
+        <div className="Messenger-button">
+          <Image
+            onClick={() => {
+              setListChat((pre: any) => {
+                return pre.filter((i: any) => i.id != value.id);
+              });
+            }}
+            alt=""
+            className="img"
+            width={30}
+            height={30}
+            src={icon.closewhite.src}
+          ></Image>
         </div>
-        <div ref={(e:HTMLDivElement )=>{
-            if(e)
-            e.scrollTop=e.scrollHeight
-        }} className='Messenger-content' >
-           {loading&& <Loading2></Loading2>}
-          {chat?.chats?.map((i:{idchat:string},index:number)=>{
-            return   <MessItem key={i.idchat} index={index} list={chat?.chats} value={i}></MessItem>
-          })}
-     
-        </div>
-        <div className='Messenger-footer'>
-         <MessInput value={value} data={chat}></MessInput>
-        </div>
+      </div>
+      <div
+        ref={(e: HTMLDivElement) => {
+          if (e) e.scrollTop = e.scrollHeight;
+        }}
+        className="Messenger-content"
+      >
+        {loading && <Loading2></Loading2>}
+        {chat?.chats?.map((i: { idchat: string }, index: number) => {
+          return (
+            <MessItem
+              key={i.idchat}
+              index={index}
+              list={chat?.chats}
+              value={i}
+            ></MessItem>
+          );
+        })}
+      </div>
+      <div className="Messenger-footer">
+        <MessInput value={value} data={chat}></MessInput>
+      </div>
     </div>
+  );
+};
+export { Messenger };
+function ListMess({ children }: { children?: any }) {
+  const { setListChat, user } = useContext(HomeContext);
+  useEffect(() => {
+    if (user)
+      getData(
+        "realtime",
+        (e: any) => {
+          if (e[0]?.user.id == user.id) {
+            setListChat((pre: any) => {
+              if (pre.find((a: { id: string }) => a.id == e[0]?.userSend.id))
+                return pre;
+              return [
+                ...pre,
+                {
+                  ...e[0]?.userSend,
+                },
+              ];
+            });
+            updateData(
+              "realtime",
+              "08ORLO3C2HSF4aCTZwcf",
+              {
+                user: {},
+                userSend: {},
+                time: moment().format("DD/MM/YYYY HH:mm"),
+              },
+              () => {},
+              () => {}
+            );
+          }
+        },
+        where(documentId(), "==", "08ORLO3C2HSF4aCTZwcf")
+      );
+  }, [user]);
+  return <div className="ListMess">{children}</div>;
 }
-export {Messenger}
- function ListMess({children}:{
-    children?:any
-}){
-    const {setListChat,user}=useContext(HomeContext)
-    useEffect(()=>{
-        if(user)
-        getData('realtime',(e:any)=>{
-           
-            if(e[0]?.user.id==user.id)
-               {
-                setListChat((pre:any)=>{
-                    if(pre.find(((a:{id:string})=>a.id==e[0]?.userSend.id))) return pre
-                    return [...pre,{
-                        ...e[0]?.userSend
-                    }]
-                })
-                updateData('realtime','08ORLO3C2HSF4aCTZwcf',{user:{},userSend:{},time:moment().format('DD/MM/YYYY HH:mm')},()=>{},()=>{})
-               }
-        },where(documentId(),'==','08ORLO3C2HSF4aCTZwcf'))
-    },[user])
-    return <div className='ListMess'>
-
-       {children}
-    </div>
-}
-export default memo(ListMess)
+export default memo(ListMess);
