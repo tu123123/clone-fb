01ad285927e7ee98754b714646b060d7e00f01ec
xuanhoa25799123 <90512018+xuanhoa25799123@users.noreply.github.com>
2024-05-05 15:06:07 +0700

    update reaction function

--- src/component/icon.tsx
+++ src/component/icon.tsx
@@ -1,44 +1,60 @@
-import bell from './assets/bell.svg';
-import group from './assets/group.svg';
-import home from './assets/home.svg';
-import logo from './assets/logo.svg';
-import market from './assets/market.svg';
-import mess from './assets/mess.svg';
-import people from './assets/people.svg';
-import video from './assets/video.svg';
-import next from './assets/next.svg'
-import menu from './assets/menu.svg'
-import like from './assets/like.svg'
-import comment from './assets/comment.svg'
-import likeWhite from './assets/likeWhite.svg'
-import share from './assets/share.svg'
-import peoplecolor from './assets/peoplecolor.svg'
-import marketcolor from './assets/marketcolor.svg'
-import loading from './assets/loading.svg'
-import closewhite from './assets/closewhite.svg'
-import close from './assets/close.svg'
-import send from './assets/send.svg'
-import camera from './assets/camera.svg'
-export const icon ={
-    bell:bell,
-    menu:menu,
-    camera:camera,
-    group:group,
-    closewhite:closewhite,
-    home:home,
-    loading:loading,
-    close:close,
-    logo:logo,
-    market:market,
-    mess:mess,
-    comment:comment,
-    share:share,
-    peoplecolor:peoplecolor,
-    marketcolor:marketcolor,
-    people:people,
-    video:video,
-    next:next,
-    like:like,
-    likeWhite:likeWhite,
-    send:send,
-}
+import bell from "./assets/bell.svg";
+import group from "./assets/group.svg";
+import home from "./assets/home.svg";
+import logo from "./assets/logo.svg";
+import market from "./assets/market.svg";
+import mess from "./assets/mess.svg";
+import people from "./assets/people.svg";
+import video from "./assets/video.svg";
+import next from "./assets/next.svg";
+import menu from "./assets/menu.svg";
+import like from "./assets/like.svg";
+import comment from "./assets/comment.svg";
+import likeWhite from "./assets/likeWhite.svg";
+import share from "./assets/share.svg";
+import peoplecolor from "./assets/peoplecolor.svg";
+import marketcolor from "./assets/marketcolor.svg";
+import loading from "./assets/loading.svg";
+import closewhite from "./assets/closewhite.svg";
+import close from "./assets/close.svg";
+import send from "./assets/send.svg";
+import camera from "./assets/camera.svg";
+import angry from "./assets/angry.svg";
+import care from "./assets/care.svg";
+import likeReaction from "./assets/like_reaction.svg";
+import love from "./assets/love.svg";
+import sad from "./assets/sad.svg";
+import wow from "./assets/wow.svg";
+import haha from "./assets/haha.svg";
+import likeblue from "./assets/likeblue.svg";
+export const icon = {
+  likeblue,
+  haha,
+  angry,
+  care,
+  likeReaction,
+  love,
+  sad,
+  wow,
+  bell: bell,
+  menu: menu,
+  camera: camera,
+  group: group,
+  closewhite: closewhite,
+  home: home,
+  loading: loading,
+  close: close,
+  logo: logo,
+  market: market,
+  mess: mess,
+  comment: comment,
+  share: share,
+  peoplecolor: peoplecolor,
+  marketcolor: marketcolor,
+  people: people,
+  video: video,
+  next: next,
+  like: like,
+  likeWhite: likeWhite,
+  send: send,
+};
---
+++ src/component/reactionIcon/index.tsx
@@ -0,0 +1,147 @@
+import { Dispatch, SetStateAction } from "react";
+import { icon } from "../icon";
+import { Tooltip } from "antd";
+import "./reactionIcon.scss";
+import { updateData } from "../firebase/config";
+import { getUser } from "../home/content";
+import React from "react";
+
+const ReactionIcon = ({
+  visible,
+  setVisible,
+  data,
+  location,
+  comment,
+}: {
+  visible: boolean;
+  setVisible: Dispatch<SetStateAction<boolean>>;
+  data: any;
+  location: string;
+  comment: any;
+}) => {
+  const updateReaction = (type: string) => {
+    let objectFind = location == "comment" ? comment : data;
+    let listReaction = [...(objectFind[type] || [])];
+    let user = getUser();
+
+    let listOldReaction: any[] = [];
+
+    let oldType = "";
+
+    Object.entries(objectFind).every((key) => {
+      if (Array.isArray(key[1]) && key[1].includes(user.id)) {
+        oldType = key[0];
+        if (oldType != type) {
+          listOldReaction = [...key[1]];
+          listOldReaction = listOldReaction.filter((x: string) => x != user.id);
+        }
+        return false;
+      }
+      return true;
+    });
+
+    if (listReaction.find((i: string) => i == user.id)) {
+      listReaction = listReaction.filter((i: string) => i != user.id);
+    }
+
+    if (oldType != type) listReaction.push(user.id);
+
+    let newData = {
+      [type]: listReaction,
+    };
+
+    if (oldType != "" && oldType != type) newData[oldType] = listOldReaction;
+
+    updateData(
+      "blog",
+      data.id,
+      location != "comment"
+        ? newData
+        : {
+            comments: data.comments.map((item: any) => {
+              if (item.idcmt == objectFind.idcmt) {
+                return {
+                  ...item,
+                  ...newData,
+                };
+              }
+              return item;
+            }),
+          },
+      () => {},
+      (e: any) => {
+        console.log(e);
+      }
+    );
+  };
+
+  return (
+    <div
+      onMouseLeave={() => {
+        setVisible(false);
+      }}
+      onMouseOver={() => {
+        if (!visible) setVisible(true);
+      }}
+      className={`icon-container ${visible ? "icon-visible" : ""} ${
+        location == "comment" ? "no-bottom" : ""
+      }`}
+    >
+      <div className="icons-content">
+        <IconItem
+          icon={icon.likeReaction.src}
+          title={"Thích"}
+          onClick={() => updateReaction("likes")}
+        />
+        <IconItem
+          icon={icon.love.src}
+          title={"Yêu thích"}
+          onClick={() => updateReaction("love")}
+        />
+        <IconItem
+          icon={icon.care.src}
+          title={"Thương thương"}
+          onClick={() => updateReaction("care")}
+        />
+        <IconItem
+          icon={icon.haha.src}
+          title={"Haha"}
+          onClick={() => updateReaction("haha")}
+        />
+        <IconItem
+          icon={icon.wow.src}
+          title={"Wow"}
+          onClick={() => updateReaction("wow")}
+        />
+        <IconItem
+          icon={icon.sad.src}
+          title={"Buồn"}
+          onClick={() => updateReaction("sad")}
+        />
+        <IconItem
+          icon={icon.angry.src}
+          title={"Phẫn nộ"}
+          onClick={() => updateReaction("angry")}
+        />
+      </div>
+    </div>
+  );
+};
+
+const IconItem = ({
+  icon,
+  title,
+  onClick,
+}: {
+  icon: string;
+  title: string;
+  onClick: () => void;
+}) => {
+  return (
+    <Tooltip arrow={false} title={title}>
+      <img src={icon} onClick={onClick} />
+    </Tooltip>
+  );
+};
+
+export default ReactionIcon;
